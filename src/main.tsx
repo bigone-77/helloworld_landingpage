5718738269e337e0003402720b8bd2b4f14052b0
Taeil Shin <106367655+bigone-77@users.noreply.github.com>
2025-07-03 14:59:37 +0900

    🐛 [Bug] 빌드 실패 해결

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 
-import "@fontsource/pretendard";
+import "@fontsource/pretendard/400.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
